7c4a5b9357af31ea5f8694bdb17a93a3581367ce
Adam Meehan <adam.meehan@gmail.com>
2019-03-28 19:38:29 +1100

    Rails uses minitest now

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,17 +3,9 @@ ENV['RAILS_ENV'] = 'test'
 
 require File.expand_path('../dummy/config/environment.rb', __FILE__)
 
-require 'test/unit'
+require 'rails/test_help'
 require 'mocha'
-
-if Rails::VERSION::MAJOR == 2
-  require 'test_help'
-else
-  require 'rails/test_help'
-  require 'mocha/test_unit'
-end
-
-require 'mocha/mini_test' if defined?(Minitest)
+require 'mocha/mini_test'
 
 require 'wicked_pdf'
 
