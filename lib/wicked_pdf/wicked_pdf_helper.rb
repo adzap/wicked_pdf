d2a1e7a209ee24ca01938caff75fa1f67fd3c020
Adam Meehan <adam.meehan@gmail.com>
2019-03-28 19:20:42 +1100

    remove stray rails 2 helper

--- lib/wicked_pdf/wicked_pdf_helper.rb
+++ lib/wicked_pdf/wicked_pdf_helper.rb
@@ -7,14 +7,5 @@ class WickedPdf
     def self.add_extension(filename, extension)
       filename.to_s.split('.').include?(extension) ? filename : "#{filename}.#{extension}"
     end
-
-    def wicked_pdf_stylesheet_link_tag(*sources)
-      css_dir = WickedPdfHelper.root_path.join('public', 'stylesheets')
-      css_text = sources.collect do |source|
-        source = WickedPdfHelper.add_extension(source, 'css')
-        "<style type='text/css'>#{File.read(css_dir.join(source))}</style>"
-      end.join("\n")
-      css_text.respond_to?(:html_safe) ? css_text.html_safe : css_text
-    end
   end
 end
